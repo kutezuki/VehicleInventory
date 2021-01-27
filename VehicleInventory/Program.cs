5086887cadb57d9f7c2f6033a6e55790a94a6dee
Osama Mansour <topsoftnation@gmail.com>
2021-01-27 13:31:15 -0500

    Initial Commit - 1/27/2021

---
+++ VehicleInventory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleInventory
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           //TODO: Read Instruction.txt file associate before you start.
+        }
+    }
+}
